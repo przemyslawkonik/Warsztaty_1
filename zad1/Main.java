4567b0da4cbf6f5acbb4dc9b06e52630c8b9e61d
Przemysław Konik <przemyslawkonik@gmail.com>
2017-11-10 13:49:48 +0100

    task 1 polishing

--- zad1/Main.java
+++ zad1/Main.java
@@ -9,28 +9,27 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		final int secretNumber = new Random().nextInt(100) + 1;
+		int inputNumber = 0;
+		final Scanner scan = new Scanner(System.in);
 
-        int secretNumber = new Random().nextInt(100) + 1;
-        int inputNumber = 0;
+		do {
+			System.out.print("Zgadnij liczbę: ");
+			try {
+				inputNumber = scan.nextInt();
 
-        do {
-            System.out.print("Zgadnij liczbę: ");
-
-            try {
-                inputNumber = new Scanner(System.in).nextInt();
-
-                if (inputNumber < secretNumber) {
-                    System.out.println("Za mało");
-                } else if (inputNumber > secretNumber) {
-                    System.out.println("Za dużo");
-                } else {
-                    System.out.println("Zgadłeś");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("To nie jest liczba");
-            }
-
-        } while (secretNumber != inputNumber);
-    }
+				if (inputNumber < secretNumber) {
+					System.out.println("Za mało");
+				} else if (inputNumber > secretNumber) {
+					System.out.println("Za dużo");
+				} else {
+					System.out.println("Zgadłeś");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("To nie jest liczba");
+			}
+		} while (secretNumber != inputNumber);
+		scan.close();
+	}
 }
