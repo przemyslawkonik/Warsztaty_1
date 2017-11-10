3ed4d21f21844569fe130db0782ec523fd8b9a42
Przemysław Konik <przemyslawkonik@gmail.com>
2017-11-10 14:32:22 +0100

    task 3 polishing

--- zad3/Main.java
+++ zad3/Main.java
@@ -8,46 +8,47 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach.");
-        int attempts = 10, min = 0, max = 1000;
-        int guess = ((max - min) / 2) + min;
+		System.out.println("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach.");
+		int attempts = 10, min = 0, max = 1000, guess;
+		Scanner scan = new Scanner(System.in);
 
-        while (attempts > 0) {
-            System.out.println("Zgaduję: " + guess);
-            System.out.println("1 - więcej");
-            System.out.println("2 - mniej");
-            System.out.println("3 - trafiłeś");
-            System.out.print("Wybierz: ");
+		while (attempts-- > 0) {
+			guess = ((max - min) / 2) + min;
+			System.out.println("Zgaduję: " + guess);
+			System.out.println("1 - więcej");
+			System.out.println("2 - mniej");
+			System.out.println("3 - trafiłeś");
+			System.out.print("Wybierz: ");
 
-            try {
-                int answer = new Scanner(System.in).nextInt();
+			try {
+				int answer = scan.nextInt();
 
-                switch (answer) {
-                    case 1: {
-                        min = guess;
-                        break;
-                    }
-                    case 2: {
-                        max = guess;
-                        break;
-                    }
-                    case 3: {
-                        System.out.println("Wygrałem");
-                        System.exit(0);
-                    }
-                    default: {
-                        System.out.println("Nie oszukuj");
-                        continue;
-                    }
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Nie oszukuj");
-            }
-
-            guess = ((max - min) / 2) + min;
-            attempts--;
-        }
-    }
+				switch (answer) {
+				case 1: {
+					min = guess;
+					break;
+				}
+				case 2: {
+					max = guess;
+					break;
+				}
+				case 3: {
+					System.out.println("Wygrałem");
+					return;
+				}
+				default: {
+					System.out.println("Nie oszukuj");
+					continue;
+				}
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Nie oszukuj");
+				scan.next();
+			}
+		}
+		System.out.println("Przegrałem");
+		scan.close();
+	}
 }
